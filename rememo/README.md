84ef9f588c17d4b86c0ed2928b935ffa74ea8bed
fanbyprinciple <fanbyprinciple@gmail.com>
2025-06-17 00:03:33 +0530

    tutorial is too good

--- rememo/README.md
+++ rememo/README.md
@@ -1,7 +1,7 @@
 # color schemes
 
-Primary: #2D3250 (Deep Indigo)
-Secondary: #424769 (Muted Dark Blue)
+Primary: #000000 (Deep Indigo)
+Secondary: #050a30 (Muted Dark Blue)
 Highlight: #7077A1 (Soft Blue-Grey)
 Accent: #CC9966 (Warm Copper/Muted Orange) - For warmth and contrast
 
--- rememo/app/auth.tsx
+++ rememo/app/auth.tsx
@@ -16,41 +16,82 @@ const AuthScreen = () => {
   const router = useRouter();
   const { signIn, signOut } = useAuth();
 
+  const checkBiometrics  = async () => {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+    if (compatible && isEnrolled) {
+      setHasBiometrics(compatible);
+    }
+  };
+
+  useEffect(() => {
+    checkBiometrics();
+  }, []);
+
+  const authenticate = async () => {
+    setIsAuthenticating(true);
+    setError(null);
+    try {
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+      const supportedTypes = await LocalAuthentication.supportedAuthenticationTypesAsync();
+      
+      const auth = await LocalAuthentication.authenticateAsync({
+        promptMessage: compatible && isEnrolled ? 'Use Face ID/ Touch ID' : 'Enter your PIN',
+        fallbackLabel: 'Enter your PIN',
+        cancelLabel: 'Cancel',
+        disableDeviceFallback: false,
+      });
+      if (auth.success) {
+        await signIn();
+        router.replace('/');
+      } else {
+        setError('Authentication failed. Please try again.');
+      }
+    } catch (err) {
+      setError('An error occurred during authentication.');
+    } finally {
+      setIsAuthenticating(false);
+    }
+  }
   return (
-    <LinearGradient colors={["#2D3250", "#424769"]} style={styles.container}>
+    <LinearGradient colors={["#000000", "#050a30"]} style={styles.container}>
       <View style={styles.viewContainer}>
         <View style={styles.iconContainer}>
-            <Ionicons name="receipt-outline" size={80} style={styles.icon} color="white" />
+            <View style={styles.iconCircle}>
+            <Ionicons name="receipt-outline" size={80} color="white" />
+            </View>
             <Text style={styles.title}  >
-            Welcome to Rememo <br />
+            Welcome to Rememo
             </Text>
             <Text style={styles.subtitle}>
               Your reminder app.
             </Text>
-            <Text style={styles.textContainer}>
-              {hasBiometrics
-                ? 'Use face/ Touch ID or PIN'
-                : 'Enter your PIN'}
-            </Text> 
-            <TouchableOpacity>
-                <Ionicons style={styles.icon} name={hasBiometrics? 'finger-print-outline' : 'keypad-outline'} size={24} color="white" />
 
-                <Text style={styles.textContainer}>
+        </View>
+        <View style={styles.card}>
+            <Text style={styles.textContainer}>
+              Please authenticate to continue
+            </Text>
+            <TouchableOpacity style={[styles.button, isAuthenticating && styles.buttonDisabled]} disabled= {isAuthenticating} onPress={authenticate}> 
+                <Ionicons style={styles.buttonIcon} name={hasBiometrics? 'finger-print-outline' : 'keypad-outline'} size={24} color="white" />
+                <Text style={styles.buttonText}>
+                  {hasBiometrics
+                    ? 'Use face/ Touch ID or PIN'
+                    : 'Enter your PIN'}
+                </Text> 
+                <Text style={styles.buttonText}>
                 {isAuthenticating ? 'Authenticating...' : hasBiometrics
-                ? 'Authenticate'
+                ? ''
                 : ''}
                 </Text>
             </TouchableOpacity>
 
             {error && 
-            <View>
+            <View style={styles.errorContainer}>
                 <Ionicons name="warning-outline" size={24} color="red" />
                 <Text style={styles.errorText}>{error}</Text>
             </View>}
-        </View>
-        
-        <View>
-            
         </View>
       </View>
     </LinearGradient>
@@ -63,24 +104,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f0f0f0',
-    padding: 20,
+    padding: 100,
   },
   viewContainer:{
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     width: width * 0.9,
-    backgroundColor: 'rgba(18, 6, 91, 0.8)',
-    borderRadius: 10,
-    padding: 20,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+   
   },
 
   iconContainer: {
@@ -117,10 +148,10 @@ errorText: {
         textAlign: 'center',
     },
     card: {
-      backgroundColor: 'rgba(12, 40, 130, 0.6)',
+      backgroundColor: 'rgba(2, 35, 142, 0.6)',
       borderRadius: 20,
       padding: 30,
-      width: width -40,
+      width: width -50,
       shadowColor: '#000',
       shadowOffset: {
         width: 0,
@@ -129,12 +160,65 @@ errorText: {
       shadowOpacity: 0.25,
       shadowRadius: 3.84,
       elevation: 5,
+      textAlign: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
     },  
-    icon: {
-        color: 'white',
-        borderRadius: 30,
-        padding: 10,
-    }
+   iconCircle: {
+    width: 120,    // Set a fixed width
+    height: 120,    // Set a fixed height (must be same as width for a perfect circle)
+      // Half of the width/height to make it a circle
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    justifyContent: 'center',  // Center the icon horizontally
+    alignItems: 'center',      // Center the icon vertically
+    // You can add more styling here, e.g., borderWidth, borderColor, shadow, etc.
+    shadowColor: '#fff',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    elevation: 5,
+    padding: 10,
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    color: 'black',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,   
+    shadowRadius: 3.84,
+    elevation: 5, 
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  errorContainer: {
+    backgroundColor: 'rgba(255, 0, 0, 0.1)',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    width: '100%',
+    alignItems: 'center',
+  }, 
+  buttonDisabled: {
+    opacity: 0.5,
+  },
+
+  buttonIcon: {
+    marginRight: 10,
+    color: 'black',
+  },
 
 });
 
